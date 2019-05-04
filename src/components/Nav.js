7fdcab1922fab391e08cf9f73bed752b40f253aa
madmaxatwork <tejasjukar@gmail.com>
2019-05-04 00:48:20 -0700

    Added Nav components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { IoIosLogOut } from 'react-icons/io';
 import { logout } from '../actions/authedUser'
+import { NavLink } from 'react-router-dom'
+
 
 class Nav extends Component {
 
@@ -14,6 +16,21 @@ class Nav extends Component {
     return (
       <nav className='nav'>
         <ul>
+        <li>
+            <NavLink to='/' exact activeClassName='active'>
+              Questions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/' exact activeClassName='active'>
+              Leaderboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/' exact activeClassName='active'>
+              New Question
+            </NavLink>
+          </li>
           <li className='user'>
           <img src={this.props.avatarURL} alt='emptyuser.jpg' className='nav-user-image'/>
           <span onClick={this.handleLogout}><IoIosLogOut size={20}/></span>
